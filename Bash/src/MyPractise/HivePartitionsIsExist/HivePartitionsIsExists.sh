5e6ebd4861a02d621982f02e9e0a7b9d721e00ca
huabingood <huabingood@qq.com>
2017-12-11 18:23:04 +0800

    判断hive中某个分区是否存在

--- Bash/src/MyPractise/HivePartitionsIsExist/HivePartitionsIsExists.sh
+++ Bash/src/MyPractise/HivePartitionsIsExist/HivePartitionsIsExists.sh
@@ -12,7 +12,8 @@ source /home/huabingood/.bashrc
 hiveDB='sdcupdb'
 hiveTable='sd_event_tbl'
 
-yesterday=`date -d "-1 day" +"%Y%m%d"`
+#yesterday=`date -d "-1 day" +"%Y%m%d"`
+yesterday='2017-10-23'
 
 
 
@@ -22,33 +23,34 @@ yesterday=`date -d "-1 day" +"%Y%m%d"`
 ## 将所有的内容按照=号分割，
 ## 通常观测到最后一个分区的值通过是最新添加的，但没有定论，这里存在bug
 ## 获取hive表中的所有分区
-#hivePartitons=$( hive -e "show partitions ${hiveDB}.${hiveTable};")
-#findPartitions=$( echo ${hivePartitons} | awk -F "=" '{print $NF}' )
+hivePartitons=$( hive -e "show partitions ${hiveDB}.${hiveTable};")
+findPartitions=$( echo ${hivePartitons} | awk -F "=" '{print $NF}' )
 
-#if [ ${findPartitions} -eq ${yesterday} ];then
-#    sleep 1800
-#    #TODO 这里调用主脚本执行
+echo ${findPartitions}
+if [ "${findPartitions}" = "${yesterday}" ];then
+    #sleep 1800
+    #TODO 这里调用主脚本执行
 
-#fi
+fi
 ###############################
 ## 方案二：
 ## 将查询出来的结果写到本地文件中，然后遍历读取文件中的行，判断查找的字符串是否存在
 
 ## 查询分区，将查询结果写到本地
-middle="/home/huabingood"
-partitionString="statdate=2017-10-23"
-hive -e "show partitions ${hiveDB}.${hiveTable};">${middle}/partitions_${yesterday}.txt
+#middle="/home/huabingood"
+#partitionString="statdate=2017-10-23"
+#hive -e "show partitions ${hiveDB}.${hiveTable};">${middle}/partitions_${yesterday}.txt
 
-while read line
-do
-    result=$( echo "${line}" | grep "${partitionString}" )
+#while read line
+#do
+#    result=$( echo "${line}" | grep "${partitionString}" )
 
-    if [ -n "${result}" ];then
-        sleep 1800
+#    if [ -n "${result}" ];then
+#        sleep 1800
 
-        TODO 这里调用主调脚本
+#        TODO 这里调用主调脚本
 
-        rm ${middle}/partitions_${yesterday}.txt
-        exit 0
-    fi
-done < ${middle}/partitions_${yesterday}.txt
+#        rm ${middle}/partitions_${yesterday}.txt
+#        exit 0
+#    fi
+#done < ${middle}/partitions_${yesterday}.txt
