3c2fae07f16ea98485a4afdb26153bae3e75733b
huabingood <huabingood@qq.com>
2018-01-08 00:41:42 +0800

    python中将科学计数法转换

--- PythonPractise/test1.py
+++ PythonPractise/test1.py
@@ -1,9 +1,13 @@
-str="1,2,3,4,abc,123.456789E4"
+
+str="1,2,3,4,123.456789E4"
 my_tuple=str.split(",")
 my_str=""
 for i in range(len(my_tuple)):
     if(my_tuple[i].find("e") or my_tuple[i].find("E")):
-        my_tuple[i]=format(my_tuple[i],'.8f')
+        # my_tuple[i]=format(my_tuple[i],'.10f')
+        my_tuple[i]="%.10f" %float(my_tuple[i])
+        print(my_tuple[i])
+    # print(type(my_tuple[i]))
 
 
 my_str=','.join(tuple(my_tuple))
