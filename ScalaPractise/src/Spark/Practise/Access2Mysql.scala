3db4e2e9f4a8bf725bd401edde198287fa3abb74
huabingood <huabingood@qq.com>
2018-02-02 18:43:51 +0800

    spark 链接数据库

---
+++ ScalaPractise/src/Spark/Practise/Access2Mysql.scala
@@ -0,0 +1,21 @@
+package Spark.Practise
+
+import java.sql.Connection
+
+object Access2Mysql {
+  def main(args:Array[String]): Unit ={
+    val url = "jdbc:mysql://huabingood01:3306/my_test"
+    val username="root"
+    val paswd = "123456"
+
+    var connection:Connection = null
+
+    try{
+
+    }
+
+
+
+  } // main
+
+}
